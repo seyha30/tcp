d9be79bf75bdfec0fe1fb09b27391452cd237c33
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-09-27 20:05:16 +0700

    m

--- TCPFileClient.java
+++ TCPFileClient.java
@@ -1,75 +1,87 @@
 import java.io.*;
 import java.net.*;
 
+import javax.swing.text.GapContent;
+
 public class TCPFileClient {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		if (!checkArgs(args)) {
 			return;
 		}
 
-		String ip = args[0];
+		String ip = "localhost";
 		String command = args[0].toLowerCase();
-		if (command.length() > 0) {
-			System.out.println("command line " + command.length());
+		Socket socket = null;
+		PrintWriter out = null;
+		final int PORT = 9999;
+		BufferedReader in = null;
+		socket = new Socket(ip, PORT);
+			switch (command) {
+			case "get":
+				String filename = args[1];
+				System.out.println("todo get");
+				try {
+					String savedfilename = System.currentTimeMillis() + "_" + filename;
+					socket.setSoTimeout(3 * 1000); // Wait for 1s for reading timeout
+					in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					out = new PrintWriter(socket.getOutputStream(), true);
+					// Send requested filename to server
+					out.println(filename);
+					// Wait for server replies whether file exists or returns file size
+					long filesize = Long.parseLong(in.readLine());
+					if (filesize < 0) {
+						System.out.println("Oop! File does not exist on server!");
+						return;
+					} else {
+						System.out.println("File size = " + filesize + " bytes");
+					}
+					out.println("OK");
+					System.out.println("Start receiving " + filename + " from server");
+					FileOutputStream outFile = new FileOutputStream(savedfilename);
+					try {
+						InputStream inSocket = socket.getInputStream();
+						int b;
+						long l = 0;
+						byte[] buf = new byte[1024];
+						while ((b = inSocket.read(buf, 0, 1024)) != -1) {
+							l += b;
+							outFile.write(buf, 0, b);
+							if (l == filesize)
+								break;
+						}
+						System.out.println("Receiving completed!");
+					} catch (SocketTimeoutException ste) {
+						System.out.println("[Error] Receiving timeout!!!!");
+					} finally {
+						outFile.close();
+					}
+				} catch (IOException ioe) {
+					System.out.println(ioe);
+				}
+				break;
+			case "put":
+				System.out.println("todo upload");
 
-		} else {
-
-		}
-
-		switch (command) {
-		case "get":
-			System.out.println("do get");
-			break;
-		case "put":
-			System.out.println("do put");
-			break;
-		default:
-			System.out.println("do quit");
-			break;
-		}
-
-//		String fileName = args[1];
-
-//		String savedfilename = "test_" + fileName;
-//		final int PORT = 9999;
-//		final String IP = "localhost";
-//        System.out.println(command);
-//		System.out.println(fileName);
-
-		/*
-		 * 
-		 * Socket socket = null; PrintWriter out = null; BufferedReader in = null; try{
-		 * socket = new Socket(IP, PORT); socket.setSoTimeout(3*1000); //Wait for 1s for
-		 * reading timeout in = new BufferedReader(new
-		 * InputStreamReader(socket.getInputStream())); out = new
-		 * PrintWriter(socket.getOutputStream(), true);
-		 * 
-		 * //Send requested filename to server out.println(filename);
-		 * 
-		 * //Wait for server replies whether file exists or returns file size long
-		 * filesize = Long.parseLong(in.readLine()); if(filesize < 0){
-		 * System.out.println("Oop! File does not exist on server!"); return; }else{
-		 * System.out.println("File size = " + filesize + " bytes"); }
-		 * 
-		 * out.println("OK");
-		 * 
-		 * System.out.println("Start receiving " + filename + " from server");
-		 * FileOutputStream outFile = new FileOutputStream(savedfilename); try{
-		 * InputStream inSocket = socket.getInputStream(); int b; long l=0; byte[] buf =
-		 * new byte[1024]; while((b = inSocket.read(buf, 0, 1024)) != -1){ l += b;
-		 * outFile.write(buf, 0, b); if(l == filesize) break; }
-		 * System.out.println("Receiving completed!"); }catch(SocketTimeoutException
-		 * ste){ System.out.println("[Error] Receiving timeout!!!!"); }finally{
-		 * outFile.close(); } }catch(IOException ioe){ System.out.println(ioe); }
-		 * 
-		 */
+				break;
+			case "quit":
+				socket.close();
+				System.out.println("quit socket");
+				break;
+			default:
+				System.out.println("command not supported");
+				break;
+			}
+		
 
 	}
 
 	private static boolean checkArgs(String[] args) {
 		if (args.length <= 0) {
-			System.out.println("Incorrect command");
+			System.out.println("Invalid command please types");
+			System.out.println("get filename");
+			System.out.println("put filename");
+			System.out.println("quit");
 			return false;
 		}
 		return true;
